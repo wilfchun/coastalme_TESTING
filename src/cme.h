c4d5948112165d0e388b62315de9a22a54b71563
Dave Favis-Mortlock <david.favis-mortlock@ouce.ox.ac.uk>
2017-10-06 15:36:09 +0100

    Tidying

--- src/cme.h
+++ src/cme.h
@@ -139,7 +139,7 @@
 
 
 //===================================================== hard-wired constants ====================================================
-string const   PROGNAME                      = "CoastalME 0.9.9 - 8 September 2017";
+string const   PROGNAME                      = "CoastalME 0.9.9 - 6 October 2017";
 string const   SHORTNAME                     = "CME";
 string const   CME_INI                       = "cme.ini";
 
