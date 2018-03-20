cb11d45b6b5d1ca492be811baee6921d82acf796
davefavismortlock <david.favis-mortlock@ouce.ox.ac.uk>
2018-03-20 11:51:01 +0000

    Tidying

--- src/cme.h
+++ src/cme.h
@@ -459,7 +459,7 @@ double const   MAX_LAND_LENGTH_OF_SHADOW_ZONE_LINE                         = 5; 
 double const   DBL_NODATA                                                  = -9999;
 
 
-string const   PROGRAM_NAME                                                = "CoastalME 0.9.9 TESTING - 19 March 2018";
+string const   PROGRAM_NAME                                                = "CoastalME 0.9.9 TESTING - 20 March 2018";
 string const   PROGRAM_NAME_SHORT                                          = "CME";
 string const   CME_INI                                                     = "cme.ini";
 
