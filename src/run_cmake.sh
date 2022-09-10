6ad770c8bde5874391d9e841a7642f0a977d841c
wilf chun <wilfchun@outlook.com>
2022-09-10 11:10:33 +0100

    Sorted makefile

--- src/run_cmake.sh
+++ src/run_cmake.sh
@@ -1,27 +1,14 @@
 #!/bin/sh
 
-# Change this to select build type
+# Change this to change build type
+buildtype=Debug
+#buildtype=Release
+#buildtype=RelWithDebInfo
+#buildtype=MinSizeRel
+#buildtype=gcov
+#buildtype=Callgrind
 
-buildtype=DEBUG
-#buildtype=RELEASE
-#buildtype=RELWITHDEBINFO     # Not yet implemented in CMakeLists.txt
-#buildtype=MINSIZEREL         # Not yet implemented in CMakeLists.txt
-#buildtype=GCOV
-#buildtype=VALGRIND
-
-# Change this to select the CShore library type
-#cshorelibrary=STATIC
-cshorelibrary=SHARED
-
-# Change this to select CShore input/output method
-#cshoreinout=FILE
-cshoreinout=ARG
-#cshoreinout=BOTH
-
-echo "CoastalME: starting CMake for Linux (using gcc, $buildtype build, $cshorelibrary CShore library, CShore input/output method=$cshoreinout)"
+echo "CoastalME: starting CMake for Linux (using gcc, $buildtype build)"
 
 rm -f CMakeCache.txt
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$buildtype -DCSHORE_LIBRARY=$cshorelibrary -DCSHORE_INOUT=$cshoreinout .
-
-echo
-echo "Finished CMake for Linux (using gcc, $buildtype build, $cshorelibrary CShore library, CShore input/output method=$cshoreinout)"
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$buildtype .
